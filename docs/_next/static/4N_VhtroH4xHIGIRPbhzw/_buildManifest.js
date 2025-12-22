8b523fbbf2aaf15a05245c31694253447d2fe9f5
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-12-22 14:11:01 +0700

    update docs

---
+++ docs/_next/static/4N_VhtroH4xHIGIRPbhzw/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/ae165637a3ab3c4f.js"
+  ],
+  "/_error": [
+    "static/chunks/c4de9e5dde461d17.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/df732c717ec4d1db.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/05ae2dce690caf6d.js",
+  "static/chunks/ab9a744cf7260773.js",
+  "static/chunks/bf3e28a161f6b9c1.css",
+  "static/chunks/turbopack-cb37ad3b9376d5bd.js"
+])
