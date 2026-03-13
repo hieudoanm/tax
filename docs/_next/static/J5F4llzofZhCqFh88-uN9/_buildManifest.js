efd7f8ad76be509278504b34a12110dac89ee42d
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-13 19:02:24 +0700

    add app page

--- docs/_next/static/J5F4llzofZhCqFh88-uN9/_buildManifest.js
+++ docs/_next/static/J5F4llzofZhCqFh88-uN9/_buildManifest.js
@@ -3,13 +3,16 @@ self.__BUILD_MANIFEST = {
     "static/chunks/7e42f8f7899b6f39.js"
   ],
   "/404": [
-    "static/chunks/a383557ae4916bb3.js"
+    "static/chunks/f6161cc93b7e2af6.js"
   ],
   "/500": [
-    "static/chunks/4bcbb732e0d27cf6.js"
+    "static/chunks/b201f388abd58b34.js"
   ],
   "/_error": [
-    "static/chunks/7765738bf5978d7c.js"
+    "static/chunks/bf349f5f9a8332dd.js"
+  ],
+  "/app": [
+    "static/chunks/af301cf7bc3d3f06.js"
   ],
   "/version": [
     "static/chunks/98b0078d5f14614d.js"
@@ -25,6 +28,7 @@ self.__BUILD_MANIFEST = {
     "/500",
     "/_app",
     "/_error",
+    "/app",
     "/version"
   ]
 };self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/_next/static/chunks/f6161cc93b7e2af6.js
+++ docs/_next/static/chunks/f6161cc93b7e2af6.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/404", [
-  "static/chunks/c92f3b3a29d7dfac.js",
+  "static/chunks/ba680ebfb2e70661.js",
   "static/chunks/3462cdd237422837.js",
   "static/chunks/df506e321eecc412.js",
-  "static/chunks/turbopack-fa613cacd02982d6.js"
+  "static/chunks/turbopack-23ce1bf3dc49a77f.js"
 ])
