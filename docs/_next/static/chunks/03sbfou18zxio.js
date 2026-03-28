ffc96c785ea94443ddec21a6f408e5c5ed534012
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-28 11:56:34 +0700

    Remove mobile

--- docs/_next/static/chunks/03sbfou18zxio.js
+++ docs/_next/static/chunks/03sbfou18zxio.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0p7bqi92ono2-.js",
   "static/chunks/0t5we65tilct7.js",
   "static/chunks/0_e6~gu7m8v92.js",
-  "static/chunks/0-w1ovx_5c_ez.css",
-  "static/chunks/turbopack-0hiajw_~8wvwu.js"
+  "static/chunks/05iae7yqa4osl.css",
+  "static/chunks/turbopack-081v2jqat410e.js"
 ])
--- packages/app/mobile/android/app/src/main/java/io/github/hieudoanm/tax/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package io.github.hieudoanm.tax;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
